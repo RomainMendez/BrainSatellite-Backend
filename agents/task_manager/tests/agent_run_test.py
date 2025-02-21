9566001aaed16f58f99f95bd4cfe392a58f17d3e
Romain Mendez <romain.mendez@protonmail.com>
2025-02-21 18:08:32 +0100

    Fixing test

--- agents/task_manager/tests/agent_run_test.py
+++ agents/task_manager/tests/agent_run_test.py
@@ -2,11 +2,17 @@ import sys
 import os
 sys.path.insert(0, os.path.abspath(os.path.join(os.path.dirname(__file__), '../../..')))
 
-from agents.task_manager.agent_run import suggest
+from agents.task_manager.agent_run import suggest, SuggestRequest
 from agents.task_manager.todo_object import Todo, TodoMemory, TodoWithInfoReturned
 
 def test_suggest():
-    returned_object = suggest("I need to buy milk !", [], [], [])
+    suggestRequest = SuggestRequest(
+        user_prompt="I need to buy milk !",
+        memories=[],
+        existing_todos=[],
+        existing_projects=[]
+    )
+    returned_object = suggest(suggestRequest)
     assert type(returned_object) == TodoWithInfoReturned
     assert returned_object.is_redundant == False
     
