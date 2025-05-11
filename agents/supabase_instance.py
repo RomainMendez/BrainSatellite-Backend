f6cee07c2af3bf2e4e016e7aa6ef2bd4a79c13a3
Romain Mendez <romain.mendez@protonmail.com>
2025-05-11 19:21:55 +0200

    Fixing the cases when the environment variables are not set up, because it was creating issues on the tests

--- agents/supabase_instance.py
+++ agents/supabase_instance.py
@@ -1,9 +1,21 @@
 import os
 from supabase import create_client, Client
 
-url: str = os.environ["SUPABASE_URL"]
-key: str = os.environ["SUPABASE_SERVICE_KEY"]
+import logging
+logger = logging.getLogger(__file__)
 
+if "SUPABASE_URL" not in os.environ or "SUPABASE_SERVICE_KEY" not in os.environ:
+    logger.warning("Failed to set up a Supabase connection, not all API calls will work !")
+    supabase: Client|None = None
+else:
+    url: str = os.environ["SUPABASE_URL"]
+    key: str = os.environ["SUPABASE_SERVICE_KEY"]
+    supabase: Client|None = create_client(url, key)
 
-supabase: Client = create_client(url, key)
+def get_supabase() -> Client:
+    if supabase != None:
+        return supabase
+    else:
+        logger.error("Failed to retrieve a Supabase instance !")
+        return None
 
