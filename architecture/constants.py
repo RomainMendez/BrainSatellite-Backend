f9d7b023aa83a638fe5b9cd72846bdb9a79cc110
Romain Mendez <romain.mendez@protonmail.com>
2025-07-08 19:07:29 +0200

    Changing LLM being targeted

--- architecture/constants.py
+++ architecture/constants.py
@@ -4,7 +4,7 @@ DEFAULT_MODEL_KWARGS = {"temperature": 0.01, "max_tokens": 5000, "top_p": 0.95, 
 SEPARATOR = " "
 DATE_FORMAT = "%Y-%m-%d" + SEPARATOR + "%H:%M"
 # To use the correct LLM API from the start
-API_BASE = "http://192.168.0.122:8080/v1"
+API_BASE = "http://192.168.0.190:8080/v1"
 #API_BASE = "http://192.168.0.185:8000/v1"
 ## Configure OpenAI settings
 ## Configuring env variable to avoid errors
